1b451a2d9ac8349185c9dbe6fda9411ee858a9f8
Skyetiz <zhouxy2602@outlook.com>
2026-02-04 16:50:16 +0800

    reactive

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,32 +1,28 @@
 <template>
     <div class="person">
-        <h2>姓名：{{name}}</h2>
-        <h2>年龄：{{age}}</h2>
+        <h2>姓名：{{person.name}}</h2>
+        <h2>年龄：{{person.age}}</h2>
         <button @click="changeName">修改姓名</button>
         <button @click="changeAge">修改年龄</button>
-        <button @click="showTel">查看联系方式</button>
     </div>
 </template>
 
-<!-- <script lang="ts"> 
-    export default{
-        name:'Person'
-    }
-</script> -->
 
-<script lang="ts" setup name="Person234">
-    let name = '张三'
-    let age = 18
-    let tel = '1893457903'
+
+<script lang="ts" setup name="Person">
+    import {reactive} from 'vue'
+
+    let person = reactive({
+        name :'张三',
+        age : 18,
+        tel :'48403489'
+    })
 
     function changeName(){
-        name = 'zhang-san'
+        person.name += '~'
     }
     function changeAge(){
-        age += 1
-    }
-    function showTel(){
-        alert(tel)
+        person.age += 1
     }
 </script>
 
