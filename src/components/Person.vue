ceaaf99df2b02b18c925e98227ef6ace727bdf50
Skyetiz <zhouxy2602@outlook.com>
2026-02-05 13:32:48 +0800

    watch4

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,31 +2,54 @@
     <div class="person">
       <h2>姓名：{{ person.name }}</h2>
       <h2>年龄：{{ person.age }}</h2>
+      <h2>汽车：{{ person.car.c1 }}、{{ person.car.c2 }}</h2>
       <button @click="changeName">修改姓名</button>
       <button @click="changeAge">修改年龄</button>
-      <button @click="changeWhole">修改全部</button>
+      <button @click="changeC1">修改车1</button>
+      <button @click="changeC2">修改车2</button>
+      <button @click="changeCar">修改整辆车</button>
+      
     </div>
 </template>
 
 <script lang="ts" setup name="Person"> 
     import {reactive,watch} from 'vue'
-
     let person = reactive({
-        name: '张三',
-        age: 18
+        name:'张三',
+        age:18,
+        car:{
+            c1:'aa',
+            c2:'11'
+        }
     })
     function changeName(){
-        person.name += '~'
+        person.name += "~"
     }
     function changeAge(){
         person.age += 1
     }
-    function changeWhole(){
-        Object.assign(person,{name:'李四',age:99})
+    function changeC1(){
+        person.car.c1 = 'bb'
     }
-    watch(person,(newVaule,oldValue)=>{
-        console.log('person变换了',newVaule,oldValue)
+    function changeC2(){
+        person.car.c2 = '22'
+    }
+    function changeCar(){
+        person.car = {c1:'cc',c2:'33'}
+    }
+
+    watch(()=> person.name,(newValue,oldValue)=>{
+        console.log("name 改变!",newValue,oldValue)
     })
+
+    // watch(person.car,(newValue,oldValue)=>{
+    //     console.log("car改变了",newValue,oldValue)
+    // })
+
+    watch(()=>person.car,(newValue,oldValue)=>{
+        console.log("car改变了",newValue,oldValue)
+    },{deep:true})
+
 </script>
 
 <style scoped>
