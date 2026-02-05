236a06bbf99e9ea19d35ee964e320275c017355f
Skyetiz <zhouxy2602@outlook.com>
2026-02-05 14:24:08 +0800

    watcheffect

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,46 +1,42 @@
 <template>
     <div class="person">
-      <h2>姓名：{{ person.name }}</h2>
-      <h2>年龄：{{ person.age }}</h2>
-      <h2>汽车：{{ person.car.c1 }}、{{ person.car.c2 }}</h2>
-      <button @click="changeName">修改姓名</button>
-      <button @click="changeAge">修改年龄</button>
-      <button @click="changeC1">修改车1</button>
-      <button @click="changeC2">修改车2</button>
-      <button @click="changeCar">修改整辆车</button>
-      
+      <h1>水温达到60度，水位到100cm提示</h1>
+      <h2>水温：{{ temp }}°</h2>
+      <h2>水位：{{ height }}cm</h2>
+      <button @click="changeTemp">修改水温</button>
+      <button @click="changeHeight">修改水位</button>
     </div>
 </template>
 
 <script lang="ts" setup name="Person"> 
-    import {reactive,watch} from 'vue'
-    let person = reactive({
-        name:'张三',
-        age:18,
-        car:{
-            c1:'aa',
-            c2:'11'
-        }
-    })
-    function changeName(){
-        person.name += "~"
+    import {ref,watch,watchEffect} from 'vue'
+
+    let temp = ref(0)
+    let height = ref(0)
+
+    function changeTemp(){
+        temp.value += 10
     }
-    function changeAge(){
-        person.age += 1
-    }
-    function changeC1(){
-        person.car.c1 = 'bb'
-    }
-    function changeC2(){
-        person.car.c2 = '22'
-    }
-    function changeCar(){
-        person.car = {c1:'cc',c2:'33'}
+    function changeHeight(){
+        height.value += 10
     }
 
-    watch(()=>[person.name,person.car],(newValue,oldValue)=>{
-        console.log("name,car改变！",newValue,oldValue)
+    // watch([temp,height],(value)=>{
+    //     let [newTemp,newHeight] = value
+    //     if (newTemp >=60 || newHeight >=100){
+    //         console.log("向服务器发送请求！")
+    //     }
+        
+    // })
+
+    watchEffect(()=>{
+        if(temp.value >= 60 || height.value >=100){
+            console.log("向服务器发送请求！")
+        }
     })
+
+ 
+
 </script>
 
 <style scoped>
